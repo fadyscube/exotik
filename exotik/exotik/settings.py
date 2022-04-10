468956878bde66a072bfce771067e7d6d5d300f4
Fady's Cube <fadyscube@gmail.com>
2022-04-10 14:54:57 +0100

    Update heroku config

--- exotik/exotik/settings.py
+++ exotik/exotik/settings.py
@@ -135,8 +135,16 @@ LOGIN_REDIRECT_URL = 'index'
 DATABASES = {'default': {}}
 
 if DEBUG == False:
-    INSTALLED_APPS += 'whitenoise.runserver_nostatic'
-    MIDDLEWARE += 'whitenoise.middleware.WhiteNoiseMiddleware'
+    MIDDLEWARE = [
+        'django.middleware.security.SecurityMiddleware',
+        "whitenoise.middleware.WhiteNoiseMiddleware",
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ]
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
     import dj_database_url
